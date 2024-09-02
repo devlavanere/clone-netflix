32a9899f9ac043a372fbd275de18ed3cb70e4b4d
Michel Lavanere <michel.lavanere@academico.ifpb.edu.br>
2024-09-02 05:20:30 -0300

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-left">
+                
+            </div>
+            <div className="navbar-right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
