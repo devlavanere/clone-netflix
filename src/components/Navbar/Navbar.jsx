5bfb4dbbc494a28ba294dd90384de2acc6b2e0c3
Michel Lavanere <lavanere.ls@gmail.com>
2024-09-02 21:20:53 -0300

    estruturando o projeto

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,35 @@
 import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="navbar-left">
-                
+                <img src={logo} alt="logo-netflix" />
+                <ul>
+                    <li>Home</li>
+                    <li>TV Shows</li>
+                    <li>Movies</li>
+                    <li>New & Popular</li>
+                    <li>My List</li>
+                    <li>Browser by Languge</li>
+                </ul>
             </div>
             <div className="navbar-right">
-
+                <img src={search_icon} alt="icon search" className='icons' />
+                <p>Children</p>
+                <img src={bell_icon} alt="icon bell" className='icons' />
+                <div className="navbar-profile">
+                    <img src={profile_img} alt="image profile" className='icons' />
+                    <img src={caret_icon} alt="icon caret" />
+                    <div className="dropdown">
+                        <p>Sign Out Netflix</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icom from '../../assets/info_icon.png'
 
 const Home = () => {
     return (
         <div className="home">
             <Navbar />
+            <div className="hero">
+                <img src={hero_banner} alt="image hero_banner" className="banner-img"/>
+                <div className="hero-caption">
+                    <img src={hero_title} alt="imagem hero-title" className="caption-img"/>
+                    <p>
+                        Discovering his ties to a secret ancient order, a young
+                        man living in modern Istambul embarks on a quest to save the 
+                        city from an immortal enemy.
+                    </p>
+                    <div className="hero-btns">
+                        <button className='btn'><img src={play_icon} alt="image play" />Play</button>
+                        <button className='btn dark-btn'><img src={info_icom} alt="image info" />More Info</button>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
