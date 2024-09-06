79a7791c5dc4a355b8c7fa01c52f53ff09c0e4e4
Michel Lavanere <michel.lavanere@academico.ifpb.edu.br>
2024-09-06 04:39:56 -0300

    estruturando footer e login

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,33 @@
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
 
 const Footer = () => {
     return (
         <div className="footer">
-
+            <div className="footer-icons">
+                <img src={facebook_icon} alt="icone facebook" />
+                <img src={instagram_icon} alt="icone instagram" />
+                <img src={twitter_icon} alt="icone twiter" />
+                <img src={youtube_icon} alt="icone youtube" />
+            </div>
+            <ul>
+                <li>Audio Description</li>
+                <li>Help Centre</li>
+                <li>Gift Cards</li>
+                <li>Media Centre</li>
+                <li>Investor Relations</li>
+                <li>Jobs</li>
+                <li>Terms of Use</li>
+                <li>Privacy</li>
+                <li>Legal Notices</li>
+                <li>Cookie Preferences</li>
+                <li>Corporate Information</li>
+                <li>Contact Us</li>
+            </ul>
+            <p className='copyright-text'>© 1997-2024 Netflix, inc.</p>
         </div>
     )
 }
