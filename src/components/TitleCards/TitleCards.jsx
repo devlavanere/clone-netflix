050f1fc5fcda84b1f6dae01874e147433fe4933e
Michel Lavanere <lavanere.ls@gmail.com>
2024-09-07 16:14:14 -0300

    modificando componentes Login e Titlecards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,20 @@
-import {  useEffect, useRef } from 'react'
+import {  useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
 const TitleCards = ({title, category}) => {
 
+    const [apiData, setApiData] = useState([])
     const cardsRef = useRef();
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MGYzNjAyNjA1NThlNGY4ZGVjM2Q0ZjJkNzEwODVjYiIsIm5iZiI6MTcyNTczMzgzMC44NzM0NzgsInN1YiI6IjY2YWIzY2U3NmJmMWM5Zjc4MzUwN2Q5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4x4ygBne7FYSbYR37qR6pLkbbIoBAmgN3_5RC4nkNvw'
+        }
+      };
 
     const handleWheel = (event) => {
         event.preventDefault();
@@ -13,6 +22,11 @@ const TitleCards = ({title, category}) => {
     }
 
     useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response => setApiData(response.results))
+        .catch(err => console.error(err));
+
         cardsRef.current.addEventListener('wheel', handleWheel);
     }, [])
 
@@ -21,10 +35,10 @@ const TitleCards = ({title, category}) => {
         <div className="title-cards">
             <h2>{title?title:"Popular on Netflix"}</h2>
             <div className="card-list" ref={cardsRef}>
-                {cards_data.map((card, index) => {
+                {apiData.map((card, index) => {
                     return <div className="card" key={index}>
-                        <img src={card.image} alt="imagem card" />
-                        <p>{card.name}</p>
+                        <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="imagem card" />
+                        <p>{card.original_title}</p>
                     </div>
                 })}
             </div>
